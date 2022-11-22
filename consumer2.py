d43f39f2d31d365f0335b35b0aba368acb193ce8
Roccolib <87078870+Roccolib@users.noreply.github.com>
2022-11-22 16:47:08 +0200

    Add files via upload

---
+++ consumer2.py
@@ -0,0 +1,36 @@
+from kafka import KafkaConsumer
+import requests
+import flask
+from flask import Flask,jsonify,json
+
+
+consumer=KafkaConsumer(
+    'topicCO2Extracted',
+    bootstrap_servers=['kafka-service.co2forumm.svc.cluster.local:9092'],
+    group_id='grouptest',
+#    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    enable_auto_commit=True
+
+)
+#lancer le module Flask à l'execution de l'api
+app = flask.Flask(__name__)
+#pas d'affichage des details des messages d erreur
+app.config["DEBUG"] = False
+#decrire l'acces a partir de root/messahesKafka limité a la fonction POST depuis le WEB
+@app.route('/Consumer2',  methods=['POST'])
+
+
+def consume_messages():
+    consumer.commit()
+    while True:
+        message_batch = consumer.poll()
+        for msg in consumer:
+            CO2 = msg.value
+            print(CO2)
+            headers = {"HTTP_HOST": "MyVeryOwnHost", "User-Agent": "topicCO2Kafka" }
+            urlConsum = 'http://orchsvc.co2forumm.svc.cluster.local:5000/orchestrator'
+            payCO2 = requests.post(url = urlConsum, data = CO2, headers = headers)
+
+if __name__ == '__main__':
+    consume_messages()
+app.run(debug=False, port=5004, host='0.0.0.0')
---
+++ orchestre.py
@@ -0,0 +1,78 @@
+import flask
+from flask import Flask,jsonify,json
+from flask import request
+import json
+import requests
+import hashlib
+
+
+#lancer le module Flask à l'execution de l'api
+app = flask.Flask(__name__)
+
+#affichage des details des messages d erreur
+app.config["DEBUG"] = True
+
+#decrire l'acces a partir de root/messahesKafka limité a la fonction POST depuis le WEB
+@app.route('/orchestrator', methods=['POST'])
+
+
+def post_orchestrator():
+
+#recuperation des metadonnées et payload reçus de la requête appelante
+ payload = request.data
+ contentType = request.headers.get('Content-Type')
+ source = request.headers.get('User-Agent')
+ topicNameDiscMessage = "topicDiscMessage"
+ topicNamePending = "topicATraiter"
+ topicNameCo2Extracted = "topicCO2Extracted"
+ print("origin = ", source)
+
+# allowed format for incoming payload
+ importables = ['application/json', 'text/plain', 'image/jpeg']
+
+#envoi le contenu du forum vers producer
+ if "discourse" in source and contentType in importables:
+  topicName = topicNameDiscMessage
+  headers = {"HTTP_HOST": "MyVeryOwnHost", "topicName": topicName, "contentType": contentType }
+  url = 'http://prodsvc.co2forumm.svc.cluster.local:5001/messageBroker'
+  postObject = requests.post(url = url, data = payload, headers = headers)
+#  print("payload, topic et contentType venu de consumer : ", payload, topicName, contentType)
+  return("Message from forum sent to producer to kafka topic xxx ")
+
+#topic venant de consumer
+ elif "topicDiscMessage" in source:
+  #1- poster vers CO2 extracted
+  payload = request.data
+#  payload = payload.decode("utf-8")
+  print("payload decode : ", payload)
+  #header à passer pour co2 extrated qui n' attend pas de topic name !
+  headers = {"HTTP_HOST": "postMessageToExtractValue" }
+  urlCO2Ext = 'http://co2xdsvc.co2forumm.svc.cluster.local:5004/CO2extracted'
+  CO2ExtObject = requests.post(url = urlCO2Ext, data = payload, headers = headers)
+  CO2 = str(CO2ExtObject.text)
+  print(CO2)
+  #return("block 2 ok")
+  #2- poster vers producer et pour post sur kafka>topicCO2Extracted
+  headers = {"HTTP_HOST": "MyVeryOwnHost", "topicName": topicNameCo2Extracted, "contentType": 'text/plain' }
+  urlProduc = 'http://prodsvc.co2forumm.svc.cluster.local:5001/messageBroker'
+  COProd = requests.post(url = urlProduc, data = CO2, headers = headers)
+  print("valeur traitee de CO2 envoyee de orchestre vers producer : ", CO2)
+  return("CO2")
+
+## elif "topicCO2Kafka" in source:
+##  payload = request.data
+##  print("payload de consumer2 ", payload)
+##  headers = {"HTTP_HOST": "postMessageToExtractValue" }
+##  urlCO2Json = 'http://vmkafka3.uksouth.cloudapp.azure.com:5005/CO2Json2Blockchain'
+##  CO2Json4Blockchain = requests.post(url = urlCO2Json, data = payload, headers = headers)
+##  CO2 = CO2Json4Blockchain.text
+##  print("j'ai recupere CO2 pour blockchain", CO2)
+##  return("Valeur de CO2 a envoyer pour blockchain: ", CO2)
+
+
+ else:
+  print("C parti vers l'etoile noire")
+  return("boucle interrompue entre Producer et Kafka alors, Bye!")
+
+if __name__ == '__main__':
+ app.run(debug=False,port=5000, host='0.0.0.0')
